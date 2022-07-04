de993d29a134459696713cbf7bd254d21362a037
Marc Vo <vo.marc@gmail.com>
2022-07-03 17:56:39 -0700

    final draft. may refactor later. add material design. improve AI

--- app-notes-unrefactored.js
+++ app-notes-unrefactored.js
@@ -13,18 +13,25 @@ const gameBoard = ( () => {
     }
 
     function reset() {
+        // you are reassigning the _board property to a new memory reference. However, the gameConditions is still referencing/pointing to the old array, hence it won't work on new rounds.
+        //this._board = ["", "", "", "", "", "", "", "", ""];
+        //this._board.length = 0; // this will cause infinite w/ the bot since random index can be 0.
         this._board.splice(0, 9, "", "", "", "", "", "", "", "", "");
     }
-
+    
+    // the Revealing Pattern: https://youtu.be/8GDk8sj0YgQ?t=668
     return {playerMove, boardStatus, _board, reset} // need _board for gameConditions 
+
 })();
 
 // controls what happens when player clicks on block and sends move to gameboard
 const displayController = ( () => {
+    // const block0 = document.querySelector("#block0");
     const setBlock = (e, symbol) => {
         e.target.textContent = symbol;
         e.target.classList.remove("empty");
         e.target.classList.add("taken");
+        // The target event property returns the element that triggered the event.
     }
 
     const setBotBlock = (botArrayIndexDATA, symbol) => {
@@ -33,7 +40,7 @@ const displayController = ( () => {
         console.log("Index: " + index);
         index.textContent = symbol;
         index.classList.remove("empty");
-        index.classList.add("taken", "fade-in-text");
+        index.classList.add("taken");
     }
 
     const clearDisplay = () => {
@@ -78,6 +85,7 @@ const gameScore = ( () => {
 // checks who's turn it is, gameboard status, and winning conditions
 const gameFlow = ( () => {    
     let symbol = "";
+    //let playerRound = 0   // IIFE will immeidatley invoke and return the playerRound on startup, therefore always 0.
     const playerTurn = () => {
             if(gameScore.playerRound == 0 || gameScore.playerRound % 2 == 0){
                 // it's player 1's turn
@@ -92,6 +100,8 @@ const gameFlow = ( () => {
     blocks.forEach((block) => {
         block.addEventListener("click", (e) => {
             let arrayIndex = block.dataset.array;
+            //console.log(e);
+            //if(gameBoard.board[arrayIndex] === ""){
             if(block.classList.contains('empty') && gameScore.playerRound !== 9){
                 playerTurn(); // whose turn and symbol is it? 
                 displayController.setBlock(e, symbol);                
@@ -108,12 +118,16 @@ const gameFlow = ( () => {
                     } else if (gameStatus === "Player O wins"){
                         gameScore.setScore2();
                     }
-                    conclusion.gameEnd();
+                    gameEnd();
                 }    
             }
             // if bot is on
             if(bot.status == "On" && gameScore.playerRound !== 9){
+            //if(block.classList.contains('empty') && gameScore.playerRound !== 9){
                 console.log("bot status == On therefore moving")
+                // playerTurn(); // whose turn and symbol is it? 
+                // console.log("bot symbol: " + symbol)
+                //symbol = "O"
                 let botMove = bot.move(); // run function to get the arrayIndex
                 console.log("after bot.move()");
                 let botArrayIndex = bot.botArrayIndex;
@@ -133,18 +147,47 @@ const gameFlow = ( () => {
                     } else if (gameStatus === "Player O wins"){
                         gameScore.setScore2();
                     }
-                    conclusion.gameEnd();
+                    gameEnd();
                 }    
             }
         });
     })
+    
+    // // why do I not need to declare/find html element first???? 
+    // gamemessages.addEventListener("click", (e) => {
+    //     if(block0.classList.contains('empty')){
+    //         playerTurn(e);
+    //         displayController.setBlock(e, symbol);
+    //         //gameBoard.board[0] = symbol;
+    //         playerRound++;
+    //     };
+    // });
+
+    //return {playerRound};
 })();
 
 const bot = ( ()=> {
     let status = "Off";
     let botArrayIndex = "";
+    
 
     function move(){
+        // will need to change the array length 0 on restart later.
+        // let boardCopy = gameBoard._board.map( (element) => {
+        //     return element;
+        // });
+        // console.table("Copy of boardgame is: " + boardCopy);
+        // console.log("boardCopy.length: " + boardCopy.length)
+
+        //skill: random
+        
+        // while (gameBoard._board[i] !== "X" || gameBoard._board[i] !== "O") {
+        //     let i = Math.floor(Math.random() * gameBoard._board.length)
+        //     //console.log(x);
+        //     console.log("module botArrayIndex: " + i)
+        //     return this.botArrayIndex = i;
+        // }
+
         let randomIndex = Math.floor(Math.random()*gameBoard._board.length);
         console.log("before if statement randomIndex: " + randomIndex)
         if(gameBoard._board[randomIndex] == "X" || gameBoard._board[randomIndex] == "O"){
@@ -152,6 +195,8 @@ const bot = ( ()=> {
         } else {
             this.botArrayIndex = randomIndex;
             console.log("else statement botArrayIndex: " + botArrayIndex)
+
+            //return botArrayIndex;
         }
         console.log(botArrayIndex)
     }
@@ -160,7 +205,9 @@ const bot = ( ()=> {
 })();
 
 // check win/lose/tie
-const gameConditions = () => {  
+const gameConditions = () => {
+    //console.log(`gameConditions ${playerRound}`)
+    
     //// player X
     // rows
     if( (gameBoard._board[0] === "X" && gameBoard._board[1] === "X" && gameBoard._board[2] === "X") ||
@@ -198,52 +245,91 @@ const gameConditions = () => {
         return "It's a tie! Click on Restart."
 };
 
-const conclusion = ( ()=> {
-    let buttonRestart = document.querySelector("#restart");
-    let buttonReset = document.querySelector("#reset")
-    
-    buttonRestart.addEventListener('click', () => {
-        restart(); 
-    });
+function gameEnd () {
+    console.log('The game is over')
+    gameScore.playerRound = 9; // disables all click event listeners.
+}
 
-    buttonReset.addEventListener('click', reset);
+// reset everything including round count
+function reset() {
+    window.location.reload(); // can't use this if we plan to keep score.
+}
 
-    // reset everything including round count
-    function reset() {
-        window.location.reload(); // can't use this if we plan to keep score.
-    };
+// try again. leave's scores alone.
+function restart(){    
+    gameBoard.reset();
+    gameBoard.boardStatus();
+    gameScore.reset();
+    displayController.clearDisplay();
+}
 
-    // try again. leave's scores alone.
-    function restart(){    
-        gameBoard.reset();
-        gameBoard.boardStatus();
-        gameScore.reset();
-        displayController.clearDisplay();
-    };
+///// factories
+// const player = (symbol, typeOfPlayer) => {
+//     let wins = 0;
+//     let playerType = typeOfPlayer;
+//     const winGame = () => {
+//         wins++;
+//         console.log(`${symbol} wins game. Games won: ${wins}`);
+//     }
+//     return { symbol, winGame }
 
-    function gameEnd () {
-        console.log('The game is over')
-        gameScore.playerRound = 9; // disables all click event listeners.
-    };
+// };
 
-    return{gameEnd};
-})();
+// const playerFirst = player("x", "human");
+// const playerSecond = player("o", "human");
+// const playerBot = player("o", "bot");
 
-// Bot button and opponent name
+
+
+
+
+// Notes:  https://youtu.be/aHrvi2zTlaU?t=898
+// also Function Factory as shown below (not to be confused w/ factory function) https://youtu.be/0aKZvNNf8BA?t=487
+// function myFunc(color) {
+//     return function(){
+//         document.body.style.color = color;
+//     }
+// }   
+// hmm his video is OnClick = xxx. In all your Index.html, it will have onclick=myFunc() as the required syntax.
+// Whereas mine below is addEventListener.  if you did buttonReset....('click', reset()), page will load and reset infinitely.... both IFFE and nonIFFE.
+
+let buttonRestart = document.querySelector("#restart");
+let buttonReset = document.querySelector("#reset")
+//butt.addEventListener('click', myFunc("red"));
+buttonRestart.addEventListener('click', () => {
+    //document.body.style.color = 'red';
+    restart(); 
+}) 
+
+buttonReset.addEventListener('click', reset);
+
+// let botSwitch = document.querySelector("#botSwitch3");
+// botSwitch.addEventListener("click", () => {
+//     bot.status = "On";
+//     console.log("Bot is now ON")
+//     let botName = document.querySelector('.player-o');
+//     botName.textContent = "HAL 9000"
+//     botName.style.color = "red"
+//     botName.style.fontWeight = "600"
+//     botSwitch.style.color = "#d3d3d3"
+//     //botSwitch.removeEventListener("click", )
+// });
+
 const botButton = ( ()=> {
     let botBtn = document.querySelector("#botSwitch")
 
-    function botButtonOn(){
-        bot.status = "On";
-        botBtn.style.color = "#d3d3d3"
-        console.log("Bot is now ON")
-        
-        let botName = document.querySelector('.player-o');
-        botName.textContent = "HAL 9000"
-        botName.style.color = "red"
-        botName.style.fontWeight = "600"
+        function botButtonOn(){
+            bot.status = "On";
+            botBtn.style.color = "#d3d3d3"
+            console.log("Bot is now ON")
+            
+            let botName = document.querySelector('.player-o');
+            botName.textContent = "HAL 9000"
+            botName.style.color = "red"
+            botName.style.fontWeight = "600"
 
-        botBtn.removeEventListener("click", botButtonOn);
-    };
-    botBtn.addEventListener("click", botButtonOn);    
+            botBtn.removeEventListener("click", botButtonOn);
+        };
+        botBtn.addEventListener("click", botButtonOn);    
 })();
+
